d49da178290919a5dfc05d1a209a24d47c698e59
Emmett Lawlor <xemmettxlawlorx@gmail.com>
2024-09-15 21:38:11 +0100

    fixed some thread errors, file names, and created connect camera functino

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import os
 import time
 import threading
 from botocore.client import Config
-
 from utils import LoggerConfig
-
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -19,112 +17,142 @@ minio_secret_key = os.getenv('MINIO_SECRET_KEY')
 minio_region_name = os.getenv('MINIO_REGION_NAME')
 rtsp_url = os.getenv('RTSP_URL')
 
+data_dir = 'output'
+os.makedirs(data_dir, exist_ok=True)
+
 logger = LoggerConfig(name='main').get_logger()
 
-try:
-    logger.info('Starting Camera-Capture')
+# Global flag to stop threads gracefully
+stop_event = threading.Event()
+
+def images_to_video(image_folder='output', output_video_name='output_video.avi', fps=30):
+    """Convert images to a video."""
+    images = [img for img in os.listdir(image_folder) if img.endswith(".jpg") and img.startswith("frame")]
+    images.sort(key=lambda x: int(x.replace('frame_', '').replace('.jpg', '')))  # Sort images by frame number
+
+    if not images:
+        logger.warning("No images found for video creation.")
+        return None
+
+    first_image_path = os.path.join(image_folder, images[0])
+    frame = cv2.imread(first_image_path)
+    height, width, layers = frame.shape
+
+    # Define the codec and create a VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    video = cv2.VideoWriter(os.path.join(image_folder, output_video_name), fourcc, fps, (width, height))
+
+    for image in images:
+        img_path = os.path.join(image_folder, image)
+        frame = cv2.imread(img_path)
+        video.write(frame)
+
+    video.release()
+    logger.info(f"Video saved as {output_video_name}")
+    return os.path.join(image_folder, output_video_name)
+
+def capture_frames(frame_lock, frame_list):
+    
+    cap = connect_camera(rtsp_url)
+
+    frame_count = 0
+    retry = 0
+    max_retry = 5
+    while not stop_event.is_set():
+        ret, frame = cap.read()
+        if not ret:
+            logger.error("Failed to read frame from camera. Retrying in 5s...")
+            retry += 1
+
+            if retry == max_retry:
+                logger.critical('Failed to read frame from camera, max retries reached.')
+                logger.info('Sleeping for five minutes.')
+                time.sleep(5*60)
+
+            time.sleep(5)
+            cap = connect_camera(rtsp_url)
+            continue
+            
+        img_path = os.path.join(data_dir, f'frame_{frame_count}.jpg')
+        cv2.imwrite(img_path, frame)
+
+        with frame_lock:
+            frame_list.append(img_path)
+
+        frame_count += 1
+        time.sleep(1)  # Adjust capture interval if needed
+    
+    cap.release()
+
+def upload_frames(s3, bucket_name, frame_lock, frame_list, upload_interval=5*60):
+    while not stop_event.is_set():
+        time.sleep(upload_interval)
+
+        with frame_lock:
+            if frame_list:
+                logger.info('Uploading frames...')
+                frame_range = (frame_list[0].split('_')[1].replace('.jpg', ''), frame_list[-1].split('_')[1].replace('.jpg', ''))
+                video_path = images_to_video(image_folder='output', output_video_name=f'{frame_range[0]}-{frame_range[1]}-video.avi')
+                if video_path:
+                    s3.upload_file(video_path, bucket_name, os.path.basename(video_path))
+                    logger.info('Video uploaded to MinIO!')
+
+                frame_list.clear()
+
+def connect_camera(rtsp_url: str):
 
     # Setup the RTSP stream
     cap = cv2.VideoCapture(rtsp_url)
     if not cap.isOpened():
-        logger.error("Failed to open camera feed")
+        logger.error("Failed to open camera feed.")
+        return
+    
+    return cap
 
-    # MinIO client configuration (using S3 compatible API)
-    s3 = boto3.client('s3',
-        endpoint_url=minio_endpoint_url,  # MinIO server URL
-        aws_access_key_id=minio_access_key,  # Your MinIO access key
-        aws_secret_access_key=minio_secret_key,  # Your MinIO secret key
-        config=Config(signature_version='s3v4'),
-        region_name=minio_region_name
-    )
-    logger.info('MinIO client configured')
+def main():
+    try:
+        logger.info('Starting Camera-Connect')
 
-    bucket_name = 'rpi-camera'
-    frame_count = 0
-    frame_list = []
-    frame_lock = threading.Lock()  # Lock for thread-safe access to frame_list
-    upload_interval = 5 * 60  # 5 minutes in seconds
+        # MinIO client configuration (using S3 compatible API)
+        s3 = boto3.client('s3',
+            endpoint_url=minio_endpoint_url,
+            aws_access_key_id=minio_access_key,
+            aws_secret_access_key=minio_secret_key,
+            config=Config(signature_version='s3v4'),
+            region_name=minio_region_name
+        )
+        logger.info('MinIO client configured')
 
-    def upload_frames():
-        """Function to upload frames every 5 minutes."""
-        global frame_list
-        while True:
-            time.sleep(upload_interval)
+        bucket_name = 'rpi-camera'
+        frame_list = []
+        frame_lock = threading.Lock()  # Thread-safe access to frame_list
 
-            # Upload frames every 5 minutes in a separate thread
-            with frame_lock:
-                if frame_list:
-                    logger.info('Uploading frames...')
-                    images_to_video(image_folder='output', output_video_name='output_video.avi')
-                    video_path = os.path.join('output', 'output_video.avi')
-                    s3.upload_file(video_path, bucket_name, 'output/output_video.avi')
-                    logger.info('Video uploaded to MinIO!')
+        # Start threads for capturing frames and uploading them
+        capture_thread = threading.Thread(target=capture_frames, args=(frame_lock, frame_list))
+        logger.info('Started Camera Thread...')
+        upload_thread = threading.Thread(target=upload_frames, args=(s3, bucket_name, frame_lock, frame_list))
+        logger.info('Started Upload Thread...')
 
-            # Clear the frame list
-            with frame_lock:
-                frame_list.clear()
+        capture_thread.start()
+        upload_thread.start()
 
-    def images_to_video(image_folder='rpi-camera', output_video_name='output_video.avi', fps=30):
-        # Get a list of all the files in the folder
-        images = [img for img in os.listdir(image_folder) if img.endswith(".jpg") and img.startswith("output")]
-        
-        # Sort images by their frame number
-        images.sort(key=lambda x: int(x.replace('output_', '').replace('.jpg', '')))  # Assuming filenames are like 'frame{i}.jpg'
-        
-        # Read the first image to get the frame size
-        frame = cv2.imread(os.path.join(image_folder, images[0]))
-        height, width, layers = frame.shape
-        
-        # Define the codec and create a VideoWriter object
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Codec for AVI format
-        video = cv2.VideoWriter(os.path.join(image_folder, output_video_name), fourcc, fps, (width, height))
-        
-        # Write each image to the video
-        for image in images:
-            img_path = os.path.join(image_folder, image)
-            frame = cv2.imread(img_path)
-            video.write(frame)
-        
-        # Release the VideoWriter object
-        video.release()
-        
-        # Print the output video name
-        print(f"Video saved as: {output_video_name}")
-
-    def capture_frames():
+        # Wait for keyboard interrupt to stop
         while True:
             try:
-                # Take a picture
-                ret, frame = cap.read()
+                time.sleep(1)
+            except KeyboardInterrupt:
+                logger.info("Keyboard interrupt received, stopping...")
+                stop_event.set()
+                break
 
-                # Save the image to the 'output' folder
-                img_path = os.path.join('output', f'frame{frame_count}.jpg')
-                cv2.imwrite(img_path, frame)
+        # Join threads when done
+        capture_thread.join()
+        upload_thread.join()
 
-                # Add frame to the list safely
-                with frame_lock:
-                    logger.info('Adding frame to list...')
-                    frame_list.append((img_path, f'frame{frame_count}.jpg'))
-                
-                # Increment frame count
-                frame_count += 1
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
+    finally:
+        logger.info("Capture released.")
 
-            except Exception as e:
-                logger.error(f"Error occurred: {e}")
-            finally:
-                cap.release()
-
-    capture_frames_thread = threading.Thread(target=capture_frames)
-    upload_frames_thread = threading.Thread(target=upload_frames)
-
-    capture_frames_thread.start()
-    upload_frames_thread.start()
-
-except Exception as e:
-    logger.error(f"Error occurred: {e}")
-finally:
-    # Stop both threads when exiting the program
-    if 'capture_frames_thread' in locals():
-        capture_frames_thread.join()
-    if 'upload_frames_thread' in locals():
-        upload_frames_thread.join()
+if __name__ == "__main__":
+    main()
