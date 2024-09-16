2b6547d3c3c96dc5f58b02d3fc5e2b4c1f88aa1a
Emmett Lawlor <xemmettxlawlorx@gmail.com>
2024-09-16 13:01:39 +0100

    updated init network

--- shell/init_network.sh
+++ shell/init_network.sh
@@ -10,7 +10,7 @@ sudo cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplica
 
 # Write new network configuration to wpa_supplicant.conf
 echo "Updating wpa_supplicant configuration..."
-echo -e "country=IE\nupdate_config=1\nctrl_interface=/var/run/wpa_supplicant\n\nnetwork={\n    ssid=\"$SSID\"\n    psk=\"$PASSWORD\"\n    key_mgmt=WPA-PSK\n}\n" | sudo tee /etc/wpa_supplicant/wpa_supplicant.conf > /dev/null
+echo -e "country=IE\nupdate_config=1\nctrl_interface=/var/run/wpa_supplicant GROUP=netdev\n\nnetwork={\n    ssid=\"$SSID\"\n    psk=\"$PASSWORD\"\n    key_mgmt=WPA-PSK\n}\n" | sudo tee /etc/wpa_supplicant/wpa_supplicant.conf > /dev/null
 
 # Restart the networking service to apply changes
 if sudo systemctl restart wpa_supplicant; then
