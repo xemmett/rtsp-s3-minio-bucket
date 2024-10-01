7b9ccca3b69521dd8a8001abb935c451a854f12a
Emmett Lawlor <xemmettxlawlorx@gmail.com>
2024-10-01 16:17:36 +0100

    rm debug

--- capture_frames.py
+++ capture_frames.py
@@ -138,12 +138,12 @@ while True:
     # ADDITIONAL DEBUG CODE TO VIEW VIDEO
     # =============================================================================
 
-    cv2.putText(frame, text, (10, 35), font, 0.75, (255, 255, 255), 2, cv2.LINE_AA)
-    frame_delta = cv2.cvtColor(frame_delta, cv2.COLOR_GRAY2BGR)
-    cv2.imshow("frame", np.hstack((frame_delta, frame))) # add to view what motion viewer sees.
+    # cv2.putText(frame, text, (10, 35), font, 0.75, (255, 255, 255), 2, cv2.LINE_AA)
+    # frame_delta = cv2.cvtColor(frame_delta, cv2.COLOR_GRAY2BGR)
+    # cv2.imshow("frame", np.hstack((frame_delta, frame))) # add to view what motion viewer sees.
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
 
 # Final cleanup
 if recording and video_writer:
